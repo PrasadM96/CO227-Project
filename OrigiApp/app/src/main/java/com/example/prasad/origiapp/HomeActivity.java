1de478cc25def3bc2af5d2e5bd27b206b06395fc
khhm <e15220khhm@gmail.com>
2019-03-28 15:52:58 +0530

    ms-sql

--- OrigiApp/app/src/main/java/com/example/prasad/origiapp/HomeActivity.java
+++ OrigiApp/app/src/main/java/com/example/prasad/origiapp/HomeActivity.java
@@ -1,44 +1,173 @@
 package com.example.prasad.origiapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
-
-import com.android.volley.Response;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class HomeActivity extends AppCompatActivity {
 
-    EditText pas,usr;
+    // Declaring layout button, edit texts
+    Button login;
+    EditText username,password;
+    ProgressBar progressBar;
+    // End Declaring layout button, edit texts
+
+    // Declaring connection variables
+    Connection con;
+    String un,pass,db,ip;
+    String usernam,passwordd;
+    //End Declaring connection variables
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        // Getting values from button, texts and progress bar
+        login = (Button) findViewById(R.id.login);
+        username = (EditText) findViewById(R.id.etSerial);
+        password = (EditText) findViewById(R.id.etAccNo);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.GONE);
+        // End Getting values from button, texts and progress bar
 
-        usr = (EditText) findViewById( R.id.et_username);
-        pas = (EditText) findViewById(R.id.et_password);
+        /*// Declaring Server ip, username, database name and password
+        ip = "192.168.1.9";
+        db = "msss";
+        un = "DESKTOP-O439JCP/user";
+        pass = "";
+        // Declaring Server ip, username, database name and password*/
 
+
+        // Setting up the function when button login is clicked
+        login.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                usernam = username.getText().toString();
+                passwordd = password.getText().toString();
+                CheckLogin checkLogin = new CheckLogin();// this is the Asynctask, which is used to process in background to reduce load on app process
+                checkLogin.execute("");
+            }
+        });
+        //End Setting up the function when button login is clicked
     }
 
-    public void login(View view){
-        String user = usr.getText().toString();
-        String pass = pas.getText().toString();
+    public class CheckLogin extends AsyncTask<String,String,String>
+    {
+        String z = "";
+        Boolean isSuccess = false;
 
-        background bg = new background(this);
-        bg.execute(user,pass);
+        @Override
+        protected void onPreExecute()
+
+        {
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String r)
+        {
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(HomeActivity.this, r, Toast.LENGTH_SHORT).show();
+            if(isSuccess)
+            {
+                Toast.makeText(HomeActivity.this , "Login Successfull" , Toast.LENGTH_LONG).show();
+                //finish();
+            }
+        }
+        @Override
+        protected String doInBackground(String... params)
+        {
+
+            if(usernam.trim().equals("")|| passwordd.trim().equals(""))
+                z = "Please enter Serial and AccNo";
+            else
+            {
+                try
+                {
+                    con = connectionclass(un, pass, db, ip);        // Connect to database
+                    if (con == null)
+                    {
+                        z = "Check Your Internet Access!";
+                    }
+                    else
+                    {
+                        String query = "SELECT * FROM [DB_A46EC4_ceb].[dbo].[CustomerMeterRelation] where MSerial= '" + usernam.toString() + "' and ConsumerAccountNo = '"+ passwordd.toString() +"' ";
+                        Statement stmt = con.createStatement();
+                        ResultSet rs = stmt.executeQuery(query);
+                        if(rs.next())
+                        {
+                            z = "Login successful";
+                            isSuccess=true;
+                            Intent homeIntent =new Intent(HomeActivity.this,RealActivity.class);
+                            startActivity(homeIntent);
+                            finish();
+                            con.close();
+                        }
+                        else
+                        {
+                            z = "Invalid Credentials!";
+                            isSuccess = false;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    isSuccess = false;
+                    z = ex.getMessage();
+                }
+            }
+            return z;
+        }
+    }
+
+
+    @SuppressLint("NewApi")
+    public Connection connectionclass(String user, String password, String database, String server)
+    {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver").newInstance();
+            ConnectionURL = "jdbc:jtds:sqlserver://sql7005.site4now.net;database=DB_A46EC4_ceb;user=DB_A46EC4_ceb_admin;password=aab-4962672";
+//            ConnectionURL = "jdbc:jtds:sqlserver://192.168.1.9;database=msss;instance=SQLEXPRESS;Network Protocol=NamedPipes" ;
+
+
+            connection = DriverManager.getConnection(ConnectionURL);
+        }
+        catch (SQLException se)
+        {
+            Log.e("error here 1 : ", se.getMessage());
+        }
+        catch (ClassNotFoundException e)
+        {
+            Log.e("error here 2 : ", e.getMessage());
+        }
+        catch (Exception e)
+        {
+            Log.e("error here 3 : ", e.getMessage());
+        }
+        return connection;
     }
 
 }
