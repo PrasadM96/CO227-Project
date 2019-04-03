f71cb5f3135d3249c446e822f9b0add80fda16c5
Prasad Madusanka <prasadmad96@gmail.com>
2019-04-03 08:25:09 +0530

    added whole project

---
+++ OrigiApp/app/src/main/java/com/example/prasad/origiapp/ConsumptionFragment.java
@@ -0,0 +1,86 @@
+package com.example.prasad.origiapp;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Axis;
+import lecho.lib.hellocharts.model.AxisValue;
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
+
+public class ConsumptionFragment extends Fragment {
+    LineChartView lineChartView;
+    String[] axisData = {"Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept",
+            "Oct", "Nov", "Dec"};
+    int[] yAxisData = {50, 20, 15, 30, 20, 60, 15, 40, 45, 10, 90, 18};
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View fragView = inflater.inflate(R.layout.fragment_consumption, container, false);
+
+       /* GraphView graph = fragView.findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3)
+        });
+        graph.addSeries(series);
+        return fragView;*/
+
+        lineChartView = fragView.findViewById(R.id.chart);
+
+        List yAxisValues = new ArrayList();
+        List axisValues = new ArrayList();
+
+
+        Line line = new Line(yAxisValues).setColor(Color.parseColor("#9C27B0"));
+
+        for (int i = 0; i < axisData.length; i++) {
+            axisValues.add(i, new AxisValue(i).setLabel(axisData[i]));
+        }
+
+        for (int i = 0; i < yAxisData.length; i++) {
+            yAxisValues.add(new PointValue(i, yAxisData[i]));
+        }
+
+        List lines = new ArrayList();
+        lines.add(line);
+
+        LineChartData data = new LineChartData();
+        data.setLines(lines);
+
+        Axis axis = new Axis();
+        axis.setValues(axisValues);
+        axis.setTextSize(16);
+        axis.setTextColor(Color.parseColor("#03A9F4"));
+        data.setAxisXBottom(axis);
+
+        Axis yAxis = new Axis();
+        yAxis.setName("Sales in millions");
+        yAxis.setTextColor(Color.parseColor("#03A9F4"));
+        yAxis.setTextSize(16);
+        data.setAxisYLeft(yAxis);
+
+
+        return fragView;
+    }
+}
---
+++ OrigiApp/app/src/main/java/com/example/prasad/origiapp/RealActivity.java
@@ -0,0 +1,92 @@
+package com.example.prasad.origiapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import android.widget.Toast;
+
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RealActivity extends AppCompatActivity {
+
+    Button logout;
+    TextView relay_status;
+
+    String relay;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_real);
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListner);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
+
+/*
+        logout = (Button) findViewById(R.id.logout);
+        relay_status = (TextView) findViewById(R.id.relay_status);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent homeIntent =new Intent(RealActivity.this,HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        });*/
+    }
+
+    private   BottomNavigationView.OnNavigationItemSelectedListener navListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment=null;
+
+                    switch(menuItem.getItemId()){
+                        case R.id.navi_home:
+                            selectedFragment =new HomeFragment();
+                            break;
+                        case R.id.navi_consumption:
+                            selectedFragment =new ConsumptionFragment();
+                            break;
+                        case R.id.navi_control:
+                            selectedFragment =new ControlFragment();
+                            break;
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+                    return true;
+                }
+            };
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.settings,menu);
+        return  true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+        int id =menuItem.getItemId();
+
+        if(id==R.id.logout){
+            startActivity(new Intent(RealActivity.this,HomeActivity.class));
+            finish();
+        }
+
+        return true;
+
+    }
+
+
+
+
+}
